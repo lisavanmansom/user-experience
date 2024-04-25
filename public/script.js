4c4d99b3015d828e0d6cde163dbbe28c7304ec6c
lisavanmansom <lisavanmansom@gmail.com>
2024-04-25 16:27:25 +0200

    nav added + playlist section

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-// hamburger menu select
+// Nav hamburger menu select ------------------------------
 let hamburger = document.querySelector('.hamburger')
 let navMenu = document.querySelector('.desktop')
 
@@ -10,7 +10,8 @@ hamburger.addEventListener("click", () => {
 
 });
 
-// external JS file GSAP
+// Carousel liked + suggested playlist --------------------
+// external JS file GSAP 
  document.addEventListener("DOMContentLoaded", (event) => {
     gsap.registerPlugin(Draggable)
    });
@@ -24,8 +25,49 @@ Draggable.create(".slayrousel", {
 // PE suggested + liked playlist section
 const body = document.querySelector('body')
 const circletext = document.querySelector('.circle-text')
+const hiddenHeader = document.querySelectorAll('.hidden-header')
 
 // JS code added & circle showing
 body.classList.add('has-js')
+body.classList.remove('suggested-no-js')
+body.classList.remove('liked-no-js')
 circletext.hidden = false
 
+// Clientside fetch -------------------------------
+let forms = document.querySelectorAll("form.like-form");
+// loop door forms
+forms.forEach(function (form) {
+
+  form.addEventListener("submit", function (event) {
+    
+    // leest data form + geeft extra eigenschap mee
+    let data = new FormData(this);
+    data.append("enhanced", true);
+
+    // client-side fetch
+    fetch(this.action, {
+      method: this.method,
+      body: new URLSearchParams(data),
+    })
+
+    // return html
+    .then(function (response) {
+      return response.text();
+    })
+
+    // Aanpassing DOM
+    .then(function (responseHTML) {
+      document.querySelector(".liked-playlist > .art-slayrousel").innerHTML = responseHTML;
+      console.log(responseHTML);
+    })
+    .catch(function (error) {
+      console.error('Error:', error);
+    });
+
+    event.preventDefault()
+
+  });
+});
+
+
+// Allstories carousel -------------------------------
