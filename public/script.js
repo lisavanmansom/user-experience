81f23bfe677fc834bda129e5ed80f49faefbdbaa
Justus Sturkenboom <justus@ju5tu5.nl>
2024-05-14 11:34:34 +0200

    POST en DELETE werkend gemaakt

--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
 // elements selecteren
 let hamburger = document.querySelector('.hamburger')
 let navMenu = document.querySelector('.desktop')
-let body = document.querySelector('body');
+let body = document.querySelector('body')
 var likedCircle = document.querySelector('.liked-circle')
 var sugCircle = document.querySelector('.sug-circle')
 var hiddenHeader = document.querySelectorAll('.hidden-header')
@@ -9,16 +9,16 @@ var prevBTN = document.querySelector('.prevBTN')
 var nextBTN = document.querySelector('.nextBTN')
 
 // hamburger menu addeventlistener
-hamburger.addEventListener("click", () => { 
- navMenu.classList.toggle('nav-active');
- navMenu.classList.toggle('desktop');
- hamburger.classList.toggle('active-bar');
-});
+hamburger.addEventListener('click', () => {
+  navMenu.classList.toggle('nav-active')
+  navMenu.classList.toggle('desktop')
+  hamburger.classList.toggle('active-bar')
+})
 
 // PE suggested + liked playlist section
 // JS code added & removing styling without JS
-body.classList.remove("liked-no-js", "sug-no-js");
-body.classList.add('has-js');
+body.classList.remove('liked-no-js', 'sug-no-js')
+body.classList.add('has-js')
 // hide content no function w/o JS
 likedCircle.hidden = false
 sugCircle.hidden = false
@@ -26,44 +26,42 @@ nextBTN.hidden = false
 prevBTN.hidden = false
 
 // Carousel liked + suggested playlist
-// external JS file GSAP 
- document.addEventListener("DOMContentLoaded", (event) => {
-    gsap.registerPlugin(Draggable)
-   });
+// external JS file GSAP
+document.addEventListener('DOMContentLoaded', (event) => {
+  gsap.registerPlugin(Draggable)
+})
 
-// GSAP draggable 
-Draggable.create(".slayrousel", {
-    type: "rotation",
-    inertia: true
-  });
+// GSAP draggable
+Draggable.create('.slayrousel', {
+  type: 'rotation',
+  inertia: true,
+})
 
-// Clientside fetch 
-let forms = document.querySelectorAll("form.like-form");
+// Clientside fetch
+// let forms = document.querySelectorAll('form.like-form')
 // loop door forms
-forms.forEach(function (form) {
+forms?.forEach(function (form) {
   form.addEventListener('submit', function (event) {
-  
     // leest data form + geeft extra eigenschap mee
-    let data = new FormData(this);
-    data.append("enhanced", true);
+    let data = new FormData(this)
+    data.append('enhanced', true)
 
     // client-side fetch
     fetch(this.action, {
       method: this.method,
       body: new URLSearchParams(data),
     })
-    // return html
-    .then(function (response) {
+      // return html
+      .then(function (response) {
         return response.text()
-    }).then(function(responseHTML) {
-        document.querySelector(".liked-playlist").innerHTML = responseHTML;
-        console.log(responseHTML);
-    })
+      })
+      .then(function (responseHTML) {
+        document.querySelector('.liked-playlist').innerHTML = responseHTML
+        console.log(responseHTML)
+      })
 
     event.preventDefault()
+  })
+})
 
-    })
-  });
-
-
-// Allstories carousel 
+// Allstories carousel
--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 // 1. Opzetten van de webserver
 
-// Importeer de zelfgemaakte functie fetchJson uit de ./helpers map
-import fetchJson from './helpers/fetch-json.js'
-
 // Importeer het npm pakket express uit de node_modules map
 import express from 'express'
+// Importeer de zelfgemaakte functie fetchJson uit de ./helpers map
+import fetchJson from './helpers/fetch-json.js'
 
 // Maak een object aan voor favorites
 let favorites = {}
@@ -26,69 +25,85 @@ app.set('views', './views')
 app.use(express.static('public'))
 
 // Zorg dat werken met request data makkelijker wordt
-app.use(express.urlencoded({ extended: true }));
-
+app.use(express.urlencoded({ extended: true }))
 
 /*** Routes & data ***/
 const storyData = await fetchJson(apiUrl + '/tm_story')
 
 // Maak een GET route voor de index
 // Stap 1
-app.get('/', function(request, response) {
+app.get('/', function (request, response) {
   response.render('index')
 })
 
 // Stap 2
-app.get('/lessons', function(request, response) {
+app.get('/lessons', function (request, response) {
   Promise.all([
-  fetchJson('https://fdnd-agency.directus.app/items/tm_story?fields=*,image.id,image.height,image.width'),
-  fetchJson('https://fdnd-agency.directus.app/items/tm_language'),
-  fetchJson('https://fdnd-agency.directus.app/items/tm_playlist?fields=*,image.id,image.height,image.width'),
-  fetchJson('https://fdnd-agency.directus.app/items/tm_audio')]).then(([storyData, languageData, playlistData, audioData]) => { 
-    
+    fetchJson('https://fdnd-agency.directus.app/items/tm_story?fields=*,image.id,image.height,image.width'),
+    fetchJson('https://fdnd-agency.directus.app/items/tm_language'),
+    fetchJson('https://fdnd-agency.directus.app/items/tm_playlist?fields=*,image.id,image.height,image.width'),
+    fetchJson('https://fdnd-agency.directus.app/items/tm_audio'),
+  ]).then(([storyData, languageData, playlistData, audioData]) => {
     // like array toegevoegd worden op de API data...
-    playlistData.data = playlistData.data.map((item)=>{
+    playlistData.data = playlistData.data.map((item) => {
       item.liked = favorites[item.id] || false
       return item
     })
 
-  response.render('lessons', {
-      stories: storyData.data, 
+    response.render('lessons', {
+      stories: storyData.data,
       language: languageData.data,
       playlist: playlistData.data,
       audio: audioData.data,
-      favorites: favorites
+      favorites: favorites,
     })
   })
 })
 
-app.get('/allstories', function(request, response) {
+app.get('/allstories', function (request, response) {
   Promise.all([
-  fetchJson('https://fdnd-agency.directus.app/items/tm_story?fields=*,image.id,image.height,image.width'),
-  fetchJson('https://fdnd-agency.directus.app/items/tm_language')]).then(([storyData, languageData]) => { 
-  response.render('allstories', {stories: storyData.data, 
-      language: languageData.data})
-  });
+    fetchJson('https://fdnd-agency.directus.app/items/tm_story?fields=*,image.id,image.height,image.width'),
+    fetchJson('https://fdnd-agency.directus.app/items/tm_language'),
+  ]).then(([storyData, languageData]) => {
+    response.render('allstories', { stories: storyData.data, language: languageData.data })
+  })
 })
 
 // Maak een POST route voor de lessons pagina
 
-app.post('/:playlistId/like-or-unlike', function(request, response) {
-    const playlistId = Number(request.params.playlistId);
-    const action = request.body.action; // Retrieve the value of the 'actie' parameter from the form
-  console.log(action, playlistId)
-    // Implement the logic to handle liking or unliking the playlist
-    if (action === 'like') {
-      // Handle 'like' action
-      favorites[playlistId] = true
-  
-    } else if (action === 'unlike') {
-      favorites[playlistId] = false
-  
-    } 
-    response.redirect(303, '/lessons')
+app.post('/:playlistId/like-or-unlike', function (request, response) {
+  const playlistId = Number(request.params.playlistId)
+  console.log(request.body.action, playlistId)
+
+  // Implement the logic to handle liking or unliking the playlist
+  if (request.body.action === 'like') {
+    // posten naar directus..
+    fetch(`${apiUrl}/tm_likes/`, {
+      method: 'POST',
+      body: JSON.stringify({
+        user: 5,
+        playlist: request.body.id,
+      }),
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+    }).then((postResponse) => {
+      console.log(postResponse)
+      response.redirect(303, '/lessons')
     })
-  
+  } else if (request.body.action === 'unlike') {
+    // deleten naar directus..
+    fetch(`${apiUrl}/tm_likes/${request.body.likeId}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+    }).then((deleteResponse) => {
+      console.log(deleteResponse)
+      response.redirect(303, '/lessons/')
+    })
+  }
+})
 
 // 3. Start de webserver
 // Stel het poortnummer in waar express op moet gaan luisteren
